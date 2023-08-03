248493041e03e4631ee5c082a12a682cc8ad1c91
feliipert <lframirez@ses.local>
2023-08-03 17:46:22 -0500

    mi primera app

--- pages/1-Predicciones.py
+++ pages/1-Predicciones.py
@@ -29,7 +29,7 @@ st.subheader(" Machine Larning modelo seleccionado")
 st.subheader(" Algoritmo de Machine Learning ")
 st.write(" Definición del algoritmo implementado para predecir la Diabetes ")
 st.subheader("Caracteristicas de entrada")
-features = ['Género 0: Hombre, 1: Mujer, 2: Otro', 'Edad', 'Hipertensión (0: No, 1: Sí). ', 'Enfermedad cardíaca (0: No, 1: Sí)', ' Tabaquismo (0: No info, 1: Nunca, 2: Fumó, 3: Fuma,4: a veces, 5: Alguna vez:  .', 'Índice de masa corporal (Valor entre 10 y 95.7) ', 'Nivel de hemoglobina A1c en la sangre (Valor entre 3.5 y 9)', 'Nivel de glucosa en la sangre (Valor entre 80 y 300)']
+features = ['Géneros 0: Hombre, 1: Mujer, 2: Otro', 'Edad', 'Hipertensión (0: No, 1: Sí). ', 'Enfermedad cardíaca (0: No, 1: Sí)', ' Tabaquismo (0: No info, 1: Nunca, 2: Fumó, 3: Fuma,4: a veces, 5: Alguna vez:  .', 'Índice de masa corporal (Valor entre 10 y 95.7) ', 'Nivel de hemoglobina A1c en la sangre (Valor entre 3.5 y 9)', 'Nivel de glucosa en la sangre (Valor entre 80 y 300)']
 st.write("A continuación, ingrese los valores de las caracteristicas de entrada")
 
 def user_input_parameters():
