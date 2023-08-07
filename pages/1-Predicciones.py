a4f61e3367d9327437163e4484902fbcc178c3be
feliipert <lframirez@ses.local>
2023-08-06 19:27:42 -0500

    mi primera app

--- pages/1-Predicciones.py
+++ pages/1-Predicciones.py
@@ -6,7 +6,9 @@ import requests
 
 def solicitud_API(muestra: list):
     #url de la API
-    url = 'http://apidiabetes.azurewebsites.net/predict'
+    #url = 'http://apidiabetes.azurewebsites.net/predict'
+    url = 'http://127.0.0.1:8000/predict'
+
     #Datos de entrada
     data = {"data": [muestra] }
     response = requests.post(url, json=data)
