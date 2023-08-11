c2c5d04e339c22c452e649f4791d9eea6c091a22
feliipert <lframirez@ses.local>
2023-08-11 11:10:01 -0500

    proyectodiabetes

--- pages/1-Descritiva.py
+++ pages/1-Descritiva.py
@@ -1,10 +1,15 @@
 import matplotlib
+import numpy as np
 import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 #Importamos los datasets que vamos autilizar
+from imblearn.over_sampling import RandomOverSampler
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 folder = 'data'
 archivo_data ='diabetes_prediction_dataset.csv'
@@ -16,7 +21,7 @@ data = pd.read_csv('data' + '/' + archivo_data, sep=',')
 #Creamos una copia de la data para reemplazar de la columna
 d=data.copy()
 # Reemplazar los valores 0  por Positivo , y 1 por Negativo
-d['diabetes'] = d['diabetes'].replace([0, 1], ['Positivo', 'Negativo'])
+#d['diabetes'] = d['diabetes'].replace([0, 1], ['Positivo', 'Negativo'])
 
 st.set_page_config(page_title="Conjunto de datos")
 st.markdown("#Conjunto de datos")
@@ -55,60 +60,137 @@ st.pyplot(fig)
 st.write("Hacemos un conteo de los datos en la featrues, para verificar que hoy valores nulos")
 st.write(d.groupby("diabetes").count())
 
-# Reemplazar los valores "Masculino" por 0 , "Femenino" por 1 y "Other" por 2 en la columna "genero"
-# Verificar si todos los valores en la columna 'gender' son numéricos o representan números enteros
-if d['gender'].apply(lambda x: isinstance(x, (int, float))).all():
-    d['gender'] = d['gender'].astype('int64')
-else:
-    st.write("No todos los valores en la columna 'gender' son numéricos o representan números enteros.")
+# Crear un botón para ejecutar el procesamiento de datos
+st.write("Ejecutamos los Botones para cambiar los datos de tipo Object a datos de Tipo Int")
+if st.button("Ejecutar Para seguir"):
+    # Reemplazar los valores en la columna 'smoking_history'
+    d['smoking_history'] = d['smoking_history'].replace(['No Info', 'never', 'former', 'current', 'not current', 'ever'], [0, 1, 2, 3, 4, 5])
+    d['gender'] = d['gender'].replace(['Male', 'Female', 'Other'], [0, 1, 2])
 
-# Mostrar el DataFrame resultante en Streamlit
-st.title("Procesamiento de Datos")
-st.write("DataFrame resultante:")
-st.write(d)
+    # Verificar y convertir la columna 'smoking_history'
+    if d['smoking_history'].apply(lambda x: isinstance(x, (int, float))).all():
+        d['smoking_history'] = d['smoking_history'].astype('int64')
+    else:
+        st.write("No todos los valores en la columna 'smoking_history' son numéricos o representan números enteros.")
+    if d['gender'].apply(lambda x: isinstance(x, (int, float))).all():
+        d['gender'] = d['gender'].astype('int64')
+    else:
+        st.write("No todos los valores en la columna 'gender' son numéricos o representan números enteros.")
 
+    # Mostrar el DataFrame resultante en Streamlit
+    st.title("Procesamiento de Datos")
+    st.write("DataFrame resultante:")
+    st.write(d)
 
+    st.write("Mostramos las estadísticas básicas en las "
+             "features: media, desviación estandar, valor mínimo, cuartiles, y valor máximo")
+    st.write(d.describe())
 
-# Reemplazar los valores smoking_history "No info" por 0 , "Never" por 1 y "former" por 2, "current" por 3, "not current" por 4, "ever" por 5 en la columna "genero"
-d['smoking_history'] = d['smoking_history'].replace(['No Info', 'never', 'former', 'current', 'not current', 'ever'], [0, 1, 2, 3, 4, 5])
+    st.write("Histograma de cada feature, para ver las distribuciones en cada feature y detectar alguna anómala o con pocos datos fuera de rango o incluso features nulas")
+    # Iterar a través de las columnas del DataFrame y mostrar los histogramas
+    for column in d.columns:
+        fig, ax = plt.subplots()
+        ax.hist(d[column], bins=20)
+        ax.set_title(f'Histograma de {column}')
+        st.pyplot(fig)
+        matplotlib.pyplot.close()
 
-if d['smoking_history'].apply(lambda x: isinstance(x, (int, float))).all():
-    d['smoking_history'] = d['smoking_history'].astype('int64')
-else:
-    print("No todos los valores en la columna son numéricos o representan números enteros.")
+    correlation_matrix = d.corr()
 
+    # Mostrar la matriz de correlación en Streamlit
+    st.title("Matriz de Correlación")
 
-st.write("Mostramos las estadísticas básicas en las "
-         "features: media, desviación estandar, valor mínimo, cuartiles, y valor máximo")
-st.write(d.describe())
-
-st.write("Histograma de cada feature, para ver las distribuciones en cada feature y detectar alguna anómala o con pocos datos fuera de rango o incluso features nulas")
-# Iterar a través de las columnas del DataFrame y mostrar los histogramas
-for column in d.columns:
+    # Utilizar Seaborn para crear un mapa de calor de la matriz de correlación
     fig, ax = plt.subplots()
-    ax.hist(d[column], bins=20)
-    ax.set_title(f'Histograma de {column}')
+    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", center=0)
     st.pyplot(fig)
-    matplotlib.pyplot.close()
 
-#correlation_matrix = d.corr()
+    # Mostrar el pairplot en Streamlit
+    st.title("Pairplot")
+    st.write("Pairplot de características")
 
-# Mostrar la matriz de correlación en Streamlit
-#st.title("Matriz de Correlación")
+    # Crear y mostrar el pairplot utilizando Seaborn
+    #pairplot = sns.pairplot(d, hue="diabetes")
+    #st.pyplot(pairplot)
 
-# Utilizar Seaborn para crear un mapa de calor de la matriz de correlación
-#fig, ax = plt.subplots()
-#sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", center=0)
-#st.pyplot(fig)
+    st.title("Proceso de división de datos de entrenamiento y testing")
+    # Obtener las features
+    features = d.drop(['diabetes'], axis=1)
+    st.write(features.head())
 
-# Pairplot
-#sns.pairplot(d, hue="diabetes")
+    # Obtener los labels
+    st.write("Obtenemos las Label")
+    labels = d['diabetes']
+    st.write(labels.head())
+    st.write(features.shape,labels.shape)
 
-# Construyendo diagramas de cajon para representar la distribucion del conjunto de datos numéricos a través de sus cuartiles
-#nrows=2
-#ncols=3
+    # Separación de la data, con un 20% para testing, y 80% para entrenamiento
+    st.write("Separación de la data, con un 20% para testing, y 80% para entrenamiento")
+    X_train, X_test, y_train, y_test = train_test_split(features, labels,
+                                                        test_size=0.20, random_state=1, stratify=labels)
+    st.write(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
-#fig = plt.figure(figsize=(22,15))
-#fig.subplots_adjust(hspace=0.2, wspace=0.1)
+    # Verificacion de la cantidad de datos para entrenamiento y para testing
+    st.write("Verificacion de la cantidad de datos para entrenamiento y para testing")
+    st.write("y_train labels unique:", np.unique(y_train, return_counts=True))
+    st.write("y_test labels unique: ", np.unique(y_test, return_counts=True))
 
+    st.title("KNN con preprocesamiento de las Features")
+    st.write("Cargamos el modelo KNN sin entrenar, despues entrenamos el modelo y posteriormente "
+             "obtenemos las metricas logradas")
+    # Cargamos el modelo KNN sin entrenar
+    model_KNN = KNeighborsClassifier(n_neighbors=3)
+    # Entrenamos el modelo KNN
+    model_KNN.fit(X_train, y_train)
+    # Obtenemos la métrica lograda
+    model_KNN.score(X_test, y_test)
+    st.write("model_KNN.score(X_test, y_test)", model_KNN.score(X_test, y_test))
+
+
+    st.subheader("Realizamos el preprocesamiento con el StandardScaler")
+    # Definir preprocesamiento
+    standard_scaler = preprocessing.StandardScaler()
+    # Preprocesar los datos
+    X_train_standard = standard_scaler.fit_transform(X_train)
+    X_test_standard = standard_scaler.transform(X_test)
+    st.write(X_train_standard[0:1])
+    st.write(X_train[0:1].to_numpy())
+
+    st.write("Cargamos el modelo KNN sin entrenar con StandardScaler, despues entrenamos el modelo y posteriormente "
+             "obtenemos las metricas logradas")
+
+    # Cargamos el modelo KNN sin entrenar
+    model_KNN = KNeighborsClassifier(n_neighbors=3)
+    # Entrenamos el modelo KNN
+    model_KNN.fit(X_train_standard, y_train)
+    # Obtenemos la métrica lograda
+    model_KNN.score(X_test_standard, y_test)
+    st.write("model_KNN.score(X_test, y_test) con StandardScaler", model_KNN.score(X_test, y_test))
+
+
+    st.subheader("Realizamos el proceso con MinMaxScaler")
+    # Definir preprocesamiento
+    min_max_scaler = preprocessing.MinMaxScaler()
+    # Preprocesar los datos
+    X_train_minmax = min_max_scaler.fit_transform(X_train)
+    X_test_minmax = min_max_scaler.transform(X_test)
+
+    st.write("Cargamos nuevamente el modelo KNN sin entrenar, despues entrenamos el modelo y posteriormente obtenemos las metricas logradas")
+    # Cargamos el modelo KNN sin entrenar
+    model_KNN = KNeighborsClassifier(n_neighbors=3)
+    # Entrenamos el modelo KNN
+    model_KNN.fit(X_train_minmax, y_train)
+    # Obtenemos la métrica lograda
+    model_KNN.score(X_test_minmax, y_test)
+    st.write("model_KNN.score(X_test, y_test) MinMax Scaler", model_KNN.score(X_test, y_test))
+
+    st.subheader("Balance de Clases")
+    st.write("Realizamos un balance de clases hacia la clase mayor con RandomOverSampler")
+    # Balance de clases hacia la clase mayor
+    sampler = RandomOverSampler(random_state=1)
+    X_train_balanced, y_train_balanced = sampler.fit_resample(X_train, y_train)
+
+    st.write("y_test  original: ", np.unique(y_test, return_counts=True))
+    st.write("y_train original: ", np.unique(y_train, return_counts=True))
+    st.write("y_train balanced: ", np.unique(y_train_balanced, return_counts=True))
 
